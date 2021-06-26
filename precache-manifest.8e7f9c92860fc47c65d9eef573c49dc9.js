6b2c42f778441dbee3dc6520180cf2c711f0e27d
Deepak Bhat <Deepak.Bhat2@cerner.com>
2021-06-26 14:48:48 +0530

    Updates

---
+++ precache-manifest.8e7f9c92860fc47c65d9eef573c49dc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa20c1a255b6dc98ca45ea3751359cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6c99f7bdead18d3cb00",
+    "url": "/static/css/main.30810e42.chunk.css"
+  },
+  {
+    "revision": "77bd5a87e2130d24e982",
+    "url": "/static/js/2.eb7a7164.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.eb7a7164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c99f7bdead18d3cb00",
+    "url": "/static/js/main.b29b4b08.chunk.js"
+  },
+  {
+    "revision": "12ed1685f73ec57702f7",
+    "url": "/static/js/runtime-main.35a91ad6.js"
+  },
+  {
+    "revision": "ed168572f7a39f5a98461387e2fa87d0",
+    "url": "/static/media/deepakbhat.ed168572.jpeg"
+  },
+  {
+    "revision": "3da8256bbebc63ae382c8754288f7efc",
+    "url": "/static/media/mphasis.3da8256b.png"
+  }
+]);
