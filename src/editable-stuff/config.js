6924be688f38a40f57647c9a82938aaf3857e11d
Shreesha Bhat <54950151+Shreesha5102@users.noreply.github.com>
2022-01-27 16:37:38 +0530

    Made necessary changes

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -7,20 +7,20 @@ const navBar = {
 // Main Body Section
 const mainBody = {
   gradientColors: "#FF9B11, #FF6A5D, #D6598B, #8E5C99, #4B5883, #2F4858, #FF7F7F, #9B59B6",
-  firstName: "Deepak",
+  firstName: "Shreesha",
   middleName: "",
   lastName: "Bhat",
-  message: "Passionate to contribute to the world of technology. ",
+  message: "Passionate Web Developer.  ",
   icons: [
     {
       description: "GitHub",
       image: "fa-github",
-      url: "https://github.com/deepakbhat-dev"
+      url: "https://github.com/Shreesha5102"
     },
     {
       description: "LinkedIn",
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/deepakbhat-dev/"
+      url: "https://linkedin.com/in/bhat-shreesha"
     }
   ]
 };
@@ -40,11 +40,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/deepakbhat.jpeg"),
+  imageLink: require("../editable-stuff/shreesha_profile_pic.jpeg"),
   imageSize: 350,
   message:
-    "Graduate of 2K18 with an engineering degree in Computer Science, adding value at present as a full-time software developer. Most passionate about giving back to the software community. Strongly believe that software is the most powerful tool in the present world to make life better and easier. In my free time, I mostly upskill or travel.",
-  resume: "https://drive.google.com/file/d/121iHvwSPJcDJUolAYZQcosC-2Xlpe3LE/view?usp=sharing"
+    "Currently a final year under-graduate at BMS College of Engineering. Passionate about learning new skills that can change the world of technolgy. I'm a gamer, crazy about motor cycles and an enthusiastic traveller.",
+  resume: "https://drive.google.com/file/d/1td8CQfgIO8zJpDDpVSD6-QYWUDHaepxq/view?usp=sharing"
 };
 
 /*
@@ -75,10 +75,9 @@ const resumeHover = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "deepakbhat-dev",
-  reposLength: 6,
-  specificRepos: [],
-  ignoreRepos: ['deepakbhat-dev', 'deepakbhat-dev.github.io'],
+  gitHubUsername: "Shreesha5102",
+  reposLength: 4,
+  specificRepos: ['HPE-nvme-health-monitoring','Event-Handler','Crossing-Dots-Problem','HangMan-Game'],
 };
 
 // LEADERSHIP Section
@@ -89,12 +88,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/deepakbhat.jpeg"), 
+      img: require("../editable-stuff/shreesha_profile_pic.jpeg"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/deepakbhat.jpeg"), 
+      img: require("../editable-stuff/shreesha_profile_pic.jpeg"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     }
@@ -110,16 +109,15 @@ const skills = {
   show: true,
   heading: "Expertise",
   hardSkills: [
-    { name: "Data Structures", value: 85 },
-    { name: "Ruby on Rails", value: 90 },
-    { name: "JavaScript", value: 90 },
-    { name: "ReactJS - Redux and Sagas", value: 85 },
-    { name: "DBMS - MySQL, MongoDB, Postgresql", value: 67 },
-    { name: "Algorithms", value: 73 },
-    { name: "CI/CD - Jenkins, Spinnaker, Docker", value: 70 },
-    { name: "Python and Django", value: 68 },
+    { name: "C / C++ ", value: 84 },
+    { name: "Python ", value: 84 },
+    { name: "JavaScript", value: 70 },
+    { name: "ReactJS", value: 75 },
+    { name: "DBMS - MySQL, MongoDB", value: 70 },
+    { name: "Data Structures", value: 80 },
+    { name: "Algorithms", value: 80 },
     { name: "NodeJS", value: 65 },
-    { name: "Amazon Web Services (AWS)", value: 57 }
+
   ],
   softSkills: [
     { name: "Collaboration", value: 90 },
@@ -138,8 +136,8 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "Have an engineering opportunity? Looking for a Full Stack Web Developer? Have any questions? or maybe, want to say hello? Please feel free to email me at",
-  email: "bhatd77@gmail.com"
+    "Currently looking for oppurtunities to boost my career. Please feel free to contact me, or just drop a 'Hi' at,",
+  email: "shreeshabhat.is18@bmsce.ac.in"
 };
 
 const experiences = {
@@ -147,23 +145,11 @@ const experiences = {
   heading: "Professional Experience",
   data: [
     {
-      company: 'granicus',
-      role: 'Software Engineer III',
-      companylogo: require('../assets/img/granicus.png'),
-      date: 'Jan 2022 – Present'
+      company: 'Hewlett-Packark Enterprises',
+      role: 'Project Intern',
+      companylogo: require('../assets/img/HPE-logo.png'),
+      date: 'April - July 2021'
     },
-    {
-      company: 'cerner',
-      role: 'Software Engineer II',
-      companylogo: require('../assets/img/cerner.png'),
-      date: 'Sept 2019 – Dec 2021'
-    },
-    {
-      company: 'mphasis',
-      role: 'Associate Software Engineer',
-      companylogo: require('../assets/img/mphasis.png'),
-      date: 'Oct 2018 – Aug 2019'
-    }
   ]
 }
 
